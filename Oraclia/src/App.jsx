963f518d00ae673a8fff903c9fd2992e9ed1c876
Léo HOTZ <cdf.leo.hotz@gmail.com>
2024-05-16 09:12:47 +0200

    Feat: Mise a jour graphique

--- Oraclia/src/App.jsx
+++ Oraclia/src/App.jsx
@@ -2,9 +2,6 @@
 import { useState } from 'react'
 import './App.css'
 import { useEffect } from 'react'
-import Typewriter from './TypeWriter'
-import { TypeAnimation } from 'react-type-animation'
-
 function App() {
 
   const defaultQuestion = 'Oraclia veux tu repondre a cette question?'
@@ -90,6 +87,7 @@ function App() {
           setIsAutoComplete(true);
         }
       }
+
     };
 
     window.addEventListener('keydown', handleKeyDown);
@@ -101,40 +99,70 @@ function App() {
   }, [inputValue]);
 
 
+  const displayTextFunction = () => {
+    if(hiddenInputValue.length<=0){
+      setDisplayText("Je n'ai pas la réponse a votre question");
+      setIsDisabled(true)
+      return;
+    }
+    else{
+      setDisplayText(hiddenInputValue)
+      setIsDisabled(true)
+    }
+  }
+
+
+  const validate = (e) => {
+    if(e.key === 'Enter'){
+      displayTextFunction()
+      
+    }
+  }
 
 
   return (
-    <div className='max-h-screen flex flex-col'>
-      <div className='flex h-24 w-full bg-black justify-center text-white items-center font-bold text-5xl'>Oraclia</div>
-      <div className='flex h-screen w-full  bg-default-background items-center justify-center p-40'>
-        <div className='h-full w-full bg-gray-900 rounded-xl flex flex-col items-center justify-center px-10 gap-5'>
-          <h1 className='text-white text-4xl'>Posez moi votre question !</h1>
-          <div className='flex gap-5 w-full justify-center items-center '>
-            <input type="text" className='text-center h-14 w-2/3 text-xl' placeholder='Votre demande' value={inputValue} onChange={handleChange} disabled={isDisabled}/>
-          </div>
-          <div className='flex gap-5 w-full justify-center items-center'>
-            <input type="text" className='text-center h-14 w-2/3 text-xl' placeholder='Question' disabled={isDisabled} value={question} onChange={(e) => setQuestion(e.target.value)} />
-          </div>
-          <div className='w-full flex justify-center text-white text-xl gap-5'>
-            <button className='bg-red-700 w-32 h-12' onClick={()=>{
-              setIsDisabled(true)
-              setDisplayText(hiddenInputValue)}}>Verifier</button>
-                          <button className='bg-red-700 w-32 h-12' onClick={()=>{
-                            setIsDisabled(false)
-                            setInputValue('')
-                            setHiddenInputValue('')
-                            setLastIndex(0)
-                            setDisplayText('')
-                            setQuestion('')
+    <>
+ <div className='flex h-24 w-full  justify-center text-white items-center font-bold text-5xl fixed top-0 left-0 z-50 bg-opacity-25'>Oraclia</div>
+    <header
+  className="relative flex items-center justify-center h-screen w-screen overflow-hidden bg-black"
+>
+<div className='h-auto w-1/2 bg-black bg-opacity-75 rounded-xl flex flex-col items-center justify-center p-10 gap-5 z-50'>
+           <h1 className='text-white text-4xl'>Posez moi votre question !</h1>
+           <div className='flex gap-5 w-full justify-center items-center '>
+             <input type="text" className='text-center h-14 w-2/3 text-xl' placeholder='Votre demande' value={inputValue} onChange={handleChange} disabled={isDisabled}/>
+           </div>
+           <div className='flex gap-5 w-full justify-center items-center'>
+             <input type="text" className='text-center h-14 w-2/3 text-xl' placeholder='Question' disabled={isDisabled} value={question} onChange={(e) => setQuestion(e.target.value)} onKeyUp={(e) => validate(e)} />
+           </div>
+           <div className='w-full flex justify-center text-white text-xl gap-5'>
+             <button className='bg-red-700 w-32 h-12' onClick={()=>{displayTextFunction()}}>Verifier</button>
+                           <button className='bg-red-700 w-32 h-12' onClick={()=>{
+                             setIsDisabled(false)
+                             setInputValue('')
+                             setHiddenInputValue('')
+                             setLastIndex(0)
+                             setDisplayText('')
+                             setQuestion('')
                             }}>Reset</button>
-          </div>
-          <div className='flex flex-col items-center w-full gap-5'>
-            <h2 className='text-white text-2xl'>Reponse</h2>
-            <p className='text-white text-xl'>{displayText}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+           </div>
+           <div className='flex flex-col items-center w-full gap-5'>
+             <h2 className='text-white text-2xl'>Reponse</h2>
+             <p className='text-white text-xl'>{displayText}</p>
+           </div>
+         </div>
+  <video
+    autoPlay
+    loop
+    muted
+    className="absolute z-10 w-auto min-w-full min-h-full "
+    >
+    <source
+      src="\src\assets\video2.mp4"
+      type="video/mp4"
+      />
+  </video>
+</header>
+      </>
   )
 }
 
